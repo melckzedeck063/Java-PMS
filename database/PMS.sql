dd0499cc5111df56b986995336080d43aa35c8fa
melckzedeck <melckzedeck063@gmail.com>
2023-09-12 10:30:27 +0300

    exported database

--- database/PMS.sql
+++ database/PMS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 31, 2023 at 09:29 AM
+-- Generation Time: Sep 12, 2023 at 09:28 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `PMS`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `brand_name` varchar(70) NOT NULL,
+  `quantity` int(5) NOT NULL,
+  `unit` varchar(50) NOT NULL,
+  `date_expired` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `date_registered` date NOT NULL DEFAULT current_timestamp(),
+  `registered_by` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `brand_name`, `quantity`, `unit`, `date_expired`, `date_registered`, `registered_by`) VALUES
+(1, 'Panadol', 'Sheladol', 20, 'pills', '2024-11-21 21:00:00', '2023-09-11', 1),
+(2, 'Septrin', 'Sheladol', 15, 'pills', '2025-01-09 21:00:00', '2023-09-11', 1),
+(3, 'Diclofenac Gel', 'Sheladol', 5, 'Gel', '2025-06-02 21:00:00', '2023-09-11', 1);
 
 -- --------------------------------------------------------
 
@@ -50,6 +76,13 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `phone`, `password`
 --
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `registered_by` (`registered_by`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -60,10 +93,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`registered_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/main/webapp/new_user.js
+++ src/main/webapp/new_user.js
@@ -1,8 +1,10 @@
 let
-    employee = document.getElementById("employee_name"),
-    address = document.getElementById("employee_address"),
-    id = document.getElementById("employeeid"),
-    contact = document.getElementById("employee_contact");
+        employee = document.getElementById("employee_name"),
+        address = document.getElementById("employee_address"),
+        id = document.getElementById("employeeid"),
+        contact = document.getElementById("employee_contact"),
+        passcode = document.getElementById("password"),
+        cpasscode = document.getElementById("cpassword");
 
 function preview() {
     document.querySelector('.prev:nth-child(1) .previewme').innerHTML = employee.value;
@@ -14,6 +16,8 @@ function preview() {
     address.value.length > 15 ? document.querySelector('.prev:nth-child(2) span i').style.opacity = "100%" : document.querySelector('.prev:nth-child(2) span i').style.opacity = "0%";
     id.value.length > 7 ? document.querySelector('.prev:nth-child(3) span i').style.opacity = "100%" : document.querySelector('.prev:nth-child(3) span i').style.opacity = "0%";
     contact.value.length > 9 ? document.querySelector('.prev:nth-child(4) span i').style.opacity = "100%" : document.querySelector('.prev:nth-child(4) span i').style.opacity = "0%";
+    passcode.value.length > 6 ? document.querySelector('.prev:nth-child(5) span i').style.opacity = "100%" : document.querySelector('.prev:nth-child(5) span i').style.opacity = "0%";
+    cpasscode.value === passcode.value ? document.querySelector('.prev:nth-child(6) span i').style.opacity = "100%" : document.querySelector('.prev:nth-child(6) span i').style.opacity = "0%";
 
 
 }
@@ -27,18 +31,25 @@ function validate() {
     id.value.length > 7 ? id.nextElementSibling.style.color = "green" : id.nextElementSibling.style.color = "red";
     contact.value.length > 9 ? contact.nextElementSibling.innerHTML = "Successiful!" : contact.nextElementSibling.innerHTML = "Enter Valid Employee Contact";
     contact.value.length > 9 ? contact.nextElementSibling.style.color = "green" : contact.nextElementSibling.style.color = "red";
+    passcode.value.length < 7 ? passcode.nextElementSibling.innerHTML = "Password Should be Greater than 6 Character" : passcode.nextElementSibling.innerHTML = "Password Accepted!";
+    passcode.value.length < 7 ? passcode.nextElementSibling.style.color = "red" : passcode.nextElementSibling.style.color = "rgb(51, 255, 0)";
 
-    if (employee.value.length > 6 && address.value.length > 15 && id.value.length > 7 && contact.value.length > 9) {
-        
-        const loading = document.createElement("div");
-        loading.setAttribute("class", "loader");
-        document.getElementById("buttonGet").innerHTML = "";
-        document.getElementById("buttonGet").append(loading);
-        setTimeout(() => {
-            window.location = "allemployee.html";
-        }, 4000);
+    cpasscode.value !== passcode.value ? cpasscode.nextElementSibling.innerHTML = "Password Do not Match!" : cpasscode.nextElementSibling.innerHTML = "Password Confirmed Successful!";
+    cpasscode.value !== passcode.value ? cpasscode.nextElementSibling.style.color = "red" : cpasscode.nextElementSibling.style.color = "rgb(51, 255, 0)";
 
-        return true;
+    if (employee.value.length > 6 && address.value.length > 15 && id.value.length > 7 && contact.value.length > 9 && passcode.value.length > 6) {
+
+        if (cpasscode.value === passcode.value) {
+            const loading = document.createElement("div");
+            loading.setAttribute("class", "loader");
+            document.getElementById("buttonGet").innerHTML = "";
+            document.getElementById("buttonGet").append(loading);
+            setTimeout(() => {
+                window.location = "allemployee.html";
+            }, 4000);
+
+            return true;
+        }
     }
     return false;
 }
